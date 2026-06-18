408d35c55d26b9994c76a6d133a712a7a771defd
KingKongs <damevhan@gmail.com>
2026-06-18 09:53:22 +0800

    Revert "Update Program.cs"
    This reverts commit 6dc2ac5f2fd6391eea346f8d5d3a460d87468ec8.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Boy");
-
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
+Console.WriteLine("Hello, din Boy");
 
